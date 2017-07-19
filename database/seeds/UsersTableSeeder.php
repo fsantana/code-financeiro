768ea101185f1e1c2047206f2e70320838d5851f
vagrant <vagrant@homestead>
2017-07-19 01:01:38 +0000

    Comiti inicial para correção

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\CodeFin\User::class,1)
+            ->states('admin')->create(
+                [
+                    'name' => 'Fernando',
+                    'email' => 'admin@user.com'
+                ]
+            );
+
+        factory(\CodeFin\User::class,1)
+            ->create(
+                [
+                    'name' => 'Client ',
+                    'email' => 'client@user.com'
+                ]
+            );
+    }
+}
