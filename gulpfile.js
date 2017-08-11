8663725348714e2efa012ac3420dc229d9666a86
Fernando Santana <fernandosantanajr@gmail.com>
2017-08-11 18:45:26 -0300

    Commit para correção de erro do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
+const gulp = require('gulp');
 const elixir = require('laravel-elixir');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('./webpack.config');
+const webpackDevConfig = require('./webpack.dev.config');
+//const mergeWebpack = require('webpack-merge');
 
 require('laravel-elixir-vue');
+require('laravel-elixir-webpack-official');
 
+// zerando os loaders para que fique somente os loaders que estão no webpackConfig
+Elixir.webpack.config.module.loaders = [];
+
+Elixir.webpack.mergeConfig(webpackConfig);
+Elixir.webpack.mergeConfig(webpackDevConfig);
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -13,7 +25,34 @@ require('laravel-elixir-vue');
  |
  */
 
+gulp.task('webpack-dev-server',()=>{
+    let config = Elixir.webpack.config;
+    new WebpackDevServer(webpack(config),{
+        proxy:{
+            '*': 'http://192.168.10.10:8000'
+        },
+        watchOptions:{
+            poll:true,
+            aggregateTimeout: 300
+        },
+        publicPath: config.output.publicPath,
+        noInfo:true,
+        stats: {colors:true}
+    }).listen(8080,"0.0.0.0",() => {
+        console.log("Bundling project ...")
+    })
+});
+
 elixir(mix => {
-    mix.sass('./resources/assets/admin/sass/admin.scss');
+    mix.sass('./resources/assets/admin/sass/admin.scss')
+        .copy('./node_modules/materialize-css/fonts/roboto','./public/fonts/roboto');
+
+    gulp.start('webpack-dev-server');
+
+    mix.browserSync({
+        host: '0.0.0.0',
+        proxy: 'http://192.168.10.10:8080',
+
+    });
       // .webpack('app.js');
 });
